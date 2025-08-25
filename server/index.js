a00ec8e13b93ac5458800bedb41b0f057964ee96
RSarvjyoti <sarvjyoti64@gmail.com>
2025-08-25 17:23:15 +0530

    database connection setup

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const connectDB = require("./src/config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 9080
+const DB_URL = process.env.MONGO_URL
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("This is a home route");
-})
+});
 
 app.listen(PORT, () =>{
+    connectDB(DB_URL);
     console.log(`Server is runnig at http://localhost:${PORT}`);
-})
+});
